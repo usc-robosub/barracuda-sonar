215ecb1481aba5be986d9530d4bf6d0847b3bc16
Russell Rodriguez <rar72937@usc.edu>
2025-08-05 17:10:43 -0700

    altimeter rpi jetson comm working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 source /opt/ros/noetic/setup.bash
 
 # Build & Source catkin_ws
-cd barracuda-sonar/catkin_ws
+cd /opt/barracuda-sonar/catkin_ws
 catkin_make
 source devel/setup.bash
 
